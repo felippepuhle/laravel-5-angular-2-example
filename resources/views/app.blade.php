6d127a2b2c23f5abbd2badf38087972f84cf7bdb
Felippe Rodrigo Puhle <felippe@dblinks.com.br>
2016-10-04 11:07:20 -0300

    Add google-site-verification meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,8 @@
         <title>Laravel 5 + Angular 2 Example</title>
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
         <base href="/"/>
+        
+        <meta name="google-site-verification" content="FQX893WD_Fsueo16KqRJ9kEtM70U-cdWfqef7h45T6o" />
     </head>
     <body>
 
